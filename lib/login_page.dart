8147b2614871b66e221457349d11f2fd1f74aa59
ARDEKA <ardekah@gmail.com>
2025-09-09 15:52:29 +0700

    update halaman login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Aplikasi Kasir Badminton',
+      title: 'Aplikasi Badminton ',
       home: const LoginPage(),
     );
   }
