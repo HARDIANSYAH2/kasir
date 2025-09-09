c5beb118a6a91870dc8af3d4f4d4a21f34363d55
ARDEKA <ardekah@gmail.com>
2025-09-09 16:46:23 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:kasir/firebase_options.dart';
+import 'package:kasir/login_page.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); 
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
 }
 
 class MyApp extends StatelessWidget {
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
     );
